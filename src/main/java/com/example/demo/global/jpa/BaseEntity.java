2d3419fb365e97b53573192954ddfb66b4aa8cf1
0n0ng <hy991006@gmail.com>
2025-09-18 15:50:12 +0900

    article domain set

---
+++ src/main/java/com/example/demo/global/jpa/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.example.demo.global.jpa;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass // 공통으로 사용하는 부모클래스이다 (테이블로 매핑이 되진 않음)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class) // 수정 자동 기록
+@ToString
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
