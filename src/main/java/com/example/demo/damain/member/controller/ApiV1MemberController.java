1230720899160d5485e2c05ba745b8cf053ed571
0n0ng <hy991006@gmail.com>
2025-09-23 16:27:55 +0900

    security

--- src/main/java/com/example/demo/damain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/demo/damain/member/controller/ApiV1MemberController.java
@@ -1,9 +1,13 @@
 package com.example.demo.damain.member.controller;
 
 
+import com.example.demo.damain.member.request.MemberRequest;
 import com.example.demo.damain.member.service.MemberService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +19,11 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Tag(name = "ApiV1MemberController", description = "회원 인증/인가 API")
 public class ApiV1MemberController {
     private final MemberService memberService;
+
+    @PostMapping("/join")
+    public String join (@Valid @RequestBody MemberRequest memberRequest) {
+        memberService.join(memberRequest.getUsername(), memberRequest.getPassword());
+
+        return "회원가입 성공";
+    }
 }
--- src/main/java/com/example/demo/damain/member/service/MemberService.java
+++ src/main/java/com/example/demo/damain/member/service/MemberService.java
@@ -3,17 +3,19 @@ package com.example.demo.damain.member.service;
 import com.example.demo.damain.member.entity.Member;
 import com.example.demo.damain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void join(String username, String password) {
         Member member = Member.builder()
                 .username(username)
-                .password(password)
+                .password(passwordEncoder.encode(password))
                 .build();
 
         memberRepository.save(member);
